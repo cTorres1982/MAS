0f8fd89ef85ae4d03d055013a63bb6896c52482c
demo <demo@gmail.com>
2020-07-08 12:37:46 -0500

    removing MVC stuff

--- src/Services/Employees.API/Controllers/EmployeesController.cs
+++ src/Services/Employees.API/Controllers/EmployeesController.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Employees.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class EmployeesController : ControllerBase
     {
